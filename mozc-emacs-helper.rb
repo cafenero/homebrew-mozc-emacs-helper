bbace2108cd5f5eb492469c2f8482d8bee2cd58b
cafenero <2006yusuke@gmail.com>
2021-07-04 13:06:01 +0900

    cleaning

--- mozc-emacs-helper.rb
+++ mozc-emacs-helper.rb
@@ -5,12 +5,10 @@ class MozcEmacsHelper < Formula
   url "https://gist.github.com/cef707ac0b23b82f399a.git"
   version "0.0.2"
 
+  depends_on "ninja"
+  # depends_on "python2"
+
   def install
-
-    # todo
-    # sdk version handling
-    # rename this package
-
     # org
     # system "svn co https://src.chromium.org/svn/trunk/tools/depot_tools"
     # system "./depot_tools/gclient config http://mozc.googlecode.com/svn/trunk/src"
@@ -19,29 +17,33 @@ class MozcEmacsHelper < Formula
     # system "python src/build_mozc.py gyp --noqt"
     # system "python src/build_mozc.py build -c Release src/mac/mac.gyp:GoogleJapaneseInput src/mac/mac.gyp:gen_launchd_confs src/unix/emacs/emacs.gyp:mozc_emacs_helper"
 
-    # you need to commads: python2, ninja
+    # OS version
+    # sw_vers | grep ProductVersion | awk '{print $2}'
+    os_version = `sw_vers | grep ProductVersion | awk '{print $2}'`
 
+    # SDK verson
+    # xcodebuild -sdk -version | grep '^MacOSX11.3.sdk' | awk '{print $4}'
+    # or
+    # ls -d /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX1* | awk -F\/ '{print $NF}' | sed -e 's/MacOSX//g' -e 's/\.sdk//g'
+    sdk_version = `xcodebuild -sdk -version | grep '^MacOSX11.3.sdk' | awk '{print $4}'`
 
-    system "cp -r /Users/yusuke/mozc ."
-    # system "git clone https://github.com/google/mozc.git -b 2018-02-26 --single-branch --recursive"
+    # debug
+    # system "cp -r ~/mozc ."
+    system "git clone https://github.com/google/mozc.git -b 2018-02-26 --single-branch --recursive"
     Dir.chdir "mozc" do
 
-      # system 'cp /Users/yusuke/mozc-emacs-helper.patch .'
-      system 'cp ../mozc-emacs-helper.patch .'
+      # debug
+      # system 'cp ~/mozc-emacs-helper.patch .'
+      system 'curl -O https://raw.githubusercontent.com/cafenero/homebrew-mozc-emacs-helper/mod_build_for_python2/mozc-emacs-helper.patch'
       system "patch -p1 < mozc-emacs-helper.patch"
 
       Dir.chdir "src" do
-        system "GYP_DEFINES='mac_sdk=11.3 mac_deployment_target=11.4' python2 build_mozc.py gyp --noqt --branding=GoogleJapaneseInput"
+        # debug
+        # system "GYP_DEFINES='mac_sdk=11.3 mac_deployment_target=11.4' python2 build_mozc.py gyp --noqt --branding=GoogleJapaneseInput"
+        system "GYP_DEFINES='mac_sdk=#{sdk_version} mac_deployment_target=#{os_version}' python2 build_mozc.py gyp --noqt --branding=GoogleJapaneseInput"
         system 'export PATH=$PATH:/usr/local/bin; python2 build_mozc.py build -c Release unix/emacs/emacs.gyp:mozc_emacs_helper'
         bin.install 'out_mac/Release/mozc_emacs_helper'
       end
     end
   end
-
-#   def caveats
-#     msg = <<-EOF.undent
-# Please restart your operating system.
-# EOF
-#   end
-
 end
