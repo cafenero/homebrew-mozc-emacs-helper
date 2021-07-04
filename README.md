38e2eed9b92300cc4605a5bcbfdb897dc8536ec9
cafenero_777 <2006yusuke@gmail.com>
2021-07-04 13:16:23 +0900

    Mod build for python2 (#1)
    * mod for python2
    
    * add patch file and github url
    
    * build success
    
    * cleaning

--- README.md
+++ README.md
@@ -2,9 +2,26 @@
 
 compile and install mozc-emacs-helper to Mac.
 
-## how to use
+## How do I install these formulae?
 
+`brew install cafenero/mozc-emacs-helper`
+
+Or `brew tap cafenero/mozc-emacs-helper` and then `brew install mozc-emacs-helper`.
+
+## Documentation
+
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
+
+
+## Debug
 ```
-$ brew tap hirocaster/homebrew-mozc-emacs-helper
-$ brew install mozc-emacs-helper
+dev
+/usr/local/Homebrew/Library/Taps/`<github account>`/homebrew-mozc-emacs-helper/
+
+log
+~/Library/Logs/Homebrew/mozc-emacs-helper
 ```
+
+## Reference
+- https://qiita.com/ignorant/items/b267bc0836010081eceb
+- https://github.com/hirocaster/homebrew-mozc-emacs-helper
--- mozc-emacs-helper.rb
+++ mozc-emacs-helper.rb
@@ -1,27 +1,49 @@
 require "formula"
 
 class MozcEmacsHelper < Formula
-  homepage "http://hiroki.jp/homebrew-mozc-emacs-helper"
+  homepage "https://github.com/cafenero/homebrew-mozc-emacs-helper"
   url "https://gist.github.com/cef707ac0b23b82f399a.git"
-  version "0.0.1"
+  version "0.0.2"
+
+  depends_on "ninja"
+  # depends_on "python2"
 
   def install
-    system "svn co http://src.chromium.org/svn/trunk/tools/depot_tools"
-    system "./depot_tools/gclient config http://mozc.googlecode.com/svn/trunk/src"
-    system "./depot_tools/gclient sync"
-    system "patch src/build_mozc.py < build_mozc.py.patch"
-    system "python src/build_mozc.py gyp --noqt"
-    system "python src/build_mozc.py build -c Release src/mac/mac.gyp:GoogleJapaneseInput src/mac/mac.gyp:gen_launchd_confs src/unix/emacs/emacs.gyp:mozc_emacs_helper"
+    # org
+    # system "svn co https://src.chromium.org/svn/trunk/tools/depot_tools"
+    # system "./depot_tools/gclient config http://mozc.googlecode.com/svn/trunk/src"
+    # system "./depot_tools/gclient sync"
+    # system "patch src/build_mozc.py < build_mozc.py.patch"
+    # system "python src/build_mozc.py gyp --noqt"
+    # system "python src/build_mozc.py build -c Release src/mac/mac.gyp:GoogleJapaneseInput src/mac/mac.gyp:gen_launchd_confs src/unix/emacs/emacs.gyp:mozc_emacs_helper"
 
-    bin.install 'src/out_mac/Release/mozc_emacs_helper'
-    system 'sudo cp -r src/out_mac/Release/Mozc.app /Library/Input\ Methods/'
-    system 'sudo cp src/out_mac/DerivedSources/Release/mac/org.mozc.inputmethod.Japanese.Converter.plist /Library/LaunchAgents'
-    system 'sudo cp src/out_mac/DerivedSources/Release/mac/org.mozc.inputmethod.Japanese.Renderer.plist /Library/LaunchAgents'
-  end
+    # OS version
+    # sw_vers | grep ProductVersion | awk '{print $2}'
+    os_version = `sw_vers | grep ProductVersion | awk '{print $2}'`
 
-  def caveats
-    msg = <<-EOF.undent
-Please restart your operating system.
-EOF
+    # SDK verson
+    # xcodebuild -sdk -version | grep '^MacOSX11.3.sdk' | awk '{print $4}'
+    # or
+    # ls -d /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX1* | awk -F\/ '{print $NF}' | sed -e 's/MacOSX//g' -e 's/\.sdk//g'
+    sdk_version = `xcodebuild -sdk -version | grep '^MacOSX11.3.sdk' | awk '{print $4}'`
+
+    # debug
+    # system "cp -r ~/mozc ."
+    system "git clone https://github.com/google/mozc.git -b 2018-02-26 --single-branch --recursive"
+    Dir.chdir "mozc" do
+
+      # debug
+      # system 'cp ~/mozc-emacs-helper.patch .'
+      system 'curl -O https://raw.githubusercontent.com/cafenero/homebrew-mozc-emacs-helper/mod_build_for_python2/mozc-emacs-helper.patch'
+      system "patch -p1 < mozc-emacs-helper.patch"
+
+      Dir.chdir "src" do
+        # debug
+        # system "GYP_DEFINES='mac_sdk=11.3 mac_deployment_target=11.4' python2 build_mozc.py gyp --noqt --branding=GoogleJapaneseInput"
+        system "GYP_DEFINES='mac_sdk=#{sdk_version} mac_deployment_target=#{os_version}' python2 build_mozc.py gyp --noqt --branding=GoogleJapaneseInput"
+        system 'export PATH=$PATH:/usr/local/bin; python2 build_mozc.py build -c Release unix/emacs/emacs.gyp:mozc_emacs_helper'
+        bin.install 'out_mac/Release/mozc_emacs_helper'
+      end
+    end
   end
 end
