2d946a7a1a734b55e1a4e61f3a66e87a507e9ed0
cafenero <2006yusuke@gmail.com>
2022-02-19 18:26:22 +0900

    bottle test 2

--- mozc-emacs-helper.rb
+++ mozc-emacs-helper.rb
@@ -2,25 +2,14 @@ require "formula"
 
 class MozcEmacsHelper < Formula
   homepage "https://github.com/cafenero/homebrew-mozc-emacs-helper"
-  url "https://github.com/cafenero/homebrew-mozc-emacs-helper/blob/add-bottle/bottle/mozc-emacs-helper--0.0.2.big_sur.bottle.1.tar.gz"
+  url "https://github.com/cafenero/homebrew-mozc-emacs-helper/raw/add-bottle/bottle/mozc-emacs-helper--0.0.2.big_sur.bottle.1.tar.gz"
+  sha256 "6d8f91b48ad8805bfec9f18e73a1d055fd7fb326d4e637f96a9762bdf53649d1"
   version "0.0.2"
-
-  bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, big_sur: "6d8f91b48ad8805bfec9f18e73a1d055fd7fb326d4e637f96a9762bdf53649d1"
-  end
 
   depends_on "ninja"
   # depends_on "python2"
 
   def install
-    # org
-    # system "svn co https://src.chromium.org/svn/trunk/tools/depot_tools"
-    # system "./depot_tools/gclient config http://mozc.googlecode.com/svn/trunk/src"
-    # system "./depot_tools/gclient sync"
-    # system "patch src/build_mozc.py < build_mozc.py.patch"
-    # system "python src/build_mozc.py gyp --noqt"
-    # system "python src/build_mozc.py build -c Release src/mac/mac.gyp:GoogleJapaneseInput src/mac/mac.gyp:gen_launchd_confs src/unix/emacs/emacs.gyp:mozc_emacs_helper"
 
     # OS version
     # sw_vers | grep ProductVersion | awk '{print $2}'
@@ -32,6 +21,14 @@ class MozcEmacsHelper < Formula
     # ls -d /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX1* | awk -F\/ '{print $NF}' | sed -e 's/MacOSX//g' -e 's/\.sdk//g'
     sdk_version = `xcodebuild -sdk -version | grep '^MacOSX11.3.sdk' | awk '{print $4}'`
 
+
+    ### binary install
+    # if os_version == 11.4 and sdk_version == 11.3
+    #   bin.install Binary => 'mozc_emacs_helper'
+    # # bin.install 'out_mac/Release/mozc_emacs_helper'
+    # else
+
+    ### source install
     # debug
     # system "cp -r ~/mozc ."
     system "git clone https://github.com/google/mozc.git -b 2018-02-26 --single-branch --recursive"
@@ -47,8 +44,12 @@ class MozcEmacsHelper < Formula
         # system "GYP_DEFINES='mac_sdk=11.3 mac_deployment_target=11.4' python2 build_mozc.py gyp --noqt --branding=GoogleJapaneseInput"
         system "GYP_DEFINES='mac_sdk=#{sdk_version} mac_deployment_target=#{os_version}' python2 build_mozc.py gyp --noqt --branding=GoogleJapaneseInput"
         system 'export PATH=$PATH:/usr/local/bin; python2 build_mozc.py build -c Release unix/emacs/emacs.gyp:mozc_emacs_helper'
-        bin.install 'out_mac/Release/mozc_emacs_helper'
+        Dir.chdir "out_mac/Release" do
+          # bin.install 'out_mac/Release/mozc_emacs_helper'
+          bin.install 'mozc_emacs_helper'
+        end
       end
     end
+
   end
 end
